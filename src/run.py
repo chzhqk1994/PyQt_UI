394b22786b3bfb26ce1b05d5b64b70644bb087de
Hyunwoo Song <chzhqk1994@naver.com>
2019-10-31 12:08:37 +0900

    load start stop video

--- src/run.py
+++ src/run.py
@@ -1,68 +1,127 @@
-from PyQt5 import QtWidgets, uic
 import sys
-import os
-import json
+import cv2
+import time
 
-default_param = {"dbAddress": "localhost",
-                 "dbUser": "user",
-                 "dbPasswd": "passwd",
-                 "redisAddress": "localhost",
-                 "redisUser": "user",
-                 "redisPasswd": "passwd"}
+from PyQt5.QtWidgets import QApplication, QFileDialog, QWidget
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtCore import QTimer
+from PyQt5 import uic
 
-actual_param = {}
-
-if not os.path.exists("parameters.json"):
-    with open('parameters.json', 'w') as param_file:
-        json.dump(default_param, param_file)
-        pass
-else:
-    with open('parameters.json') as param_file:
-        actual_param = json.load(param_file)
+form_class1 = uic.loadUiType("main.ui")[0]
 
 
-class MainWindow(QtWidgets.QMainWindow):
+class App(QWidget):
     def __init__(self):
-        super(MainWindow, self).__init__()  # call the inherited classes __init__ method
-        uic.loadUi('main.ui', self)
+        super().__init__()
+        self.title = 'Find video file'
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 480
+        self.initUI()
 
-        self.set_ui_text(actual_param)
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.openFileNameDialog()
 
-        self.save_bt.clicked.connect(self.save)
-        self.reset_bt.clicked.connect(self.reset)
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        self.fileName, _ = QFileDialog.getOpenFileName(self, "Find video file", "",
+                                                       "All Files (*);;Python Files (*.py)", options=options)
 
-    def set_ui_text(self, dict):
-        self.db_ip_text.setText(dict['dbAddress'])
-        self.db_user_text.setText(dict['dbUser'])
-        self.db_passwd_text.setText(dict['dbPasswd'])
-        self.redis_ip_text.setText(dict['redisAddress'])
-        self.redis_port_text.setText(dict['redisUser'])
-        self.redis_db_name_text.setText(dict['redisPasswd'])
 
-    def save(self):
-        actual_param['dbAddress'] = self.db_ip_text.toPlainText()
-        actual_param['dbUser'] = self.db_user_text.toPlainText()
-        actual_param['dbPasswd'] = self.db_passwd_text.toPlainText()
-        actual_param['redisAddress'] = self.redis_ip_text.toPlainText()
-        actual_param['redisUser'] = self.redis_port_text.toPlainText()
-        actual_param['redisPasswd'] = self.redis_db_name_text.toPlainText()
+class MainWindow(QWidget, form_class1):
+    def __init__(self):
+        # call QWidget constructor
+        super().__init__()
+        self.setupUi(self)
 
-        with open('parameters.json', 'w') as param_file:
-            json.dump(actual_param, param_file)
+        self.cap = None
+        self.fileName = ''
 
-    def reset(self):
-        if not os.path.exists("parameters.json"):
-            with open('parameters.json', 'w') as param_file:
-                json.dump(default_param, param_file)
+        self.find_video_bt.clicked.connect(self.findVideoFile)
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.viewCam)
+        self.start_bt.clicked.connect(self.controlTimer)
+
+    # video 를 선택하기 위한 창을 보여줌
+    def findVideoFile(self):
+        self.app = App()
+        self.app.show()
+        self.video_file_path_label.setText(self.app.fileName)
+        self.fileName = self.app.fileName
+        self.app.close()
+        if self.app.fileName == '':
+            return
+
+        self.cap = cv2.VideoCapture(self.app.fileName)
+        # video file load 후 첫 번재 프레임을 화면에 출력
+        ret, frame = self.cap.read()
+        try:
+            self.viewer_drawer(frame)
+        except TypeError:
+            pass
+
+    def viewCam(self):
+        # prev_time = time.time()
+        # cur_time = time.time()
+        # sec = cur_time - prev_time
+        #
+        # self.cap.set(cv2.CAP_PROP_POS_FRAMES, sec * 30)
+        ret, frame = self.cap.read()
+
+        if ret:
+
+            # get image infos
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            height, width, channel = frame.shape
+            step = channel * width
+            # create QImage from image
+            qImg = QImage(frame.data, width, height, step, QImage.Format_RGB888)
+
+            width = self.image_label.geometry().getRect()[2]
+            height = self.image_label.geometry().getRect()[3]
+
+            self.image_label.setPixmap(QPixmap.fromImage(qImg).scaled(width, height))
+
         else:
-            with open('parameters.json', 'w') as param_file:
-                json.dump(default_param, param_file)
+            self.timer.stop()
+            self.cap.release()
+            self.start_bt.setText("Start")
 
-        self.set_ui_text(default_param)
+    def viewer_drawer(self, frame):
+        try:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            height, width, channel = frame.shape
+            step = channel * width
+            qImg = QImage(frame.data, width, height, step, QImage.Format_RGB888)
+
+            # for resize
+            width = self.image_label.geometry().getRect()[2]
+            height = self.image_label.geometry().getRect()[3]
+            self.image_label.setPixmap(QPixmap.fromImage(qImg).scaled(width, height))
+
+        except:
+            pass
+
+    # start/stop timer
+    def controlTimer(self):
+        if not self.timer.isActive():
+            self.cap = cv2.VideoCapture(self.fileName)
+            self.timer.start(20)
+            self.start_bt.setText("Stop")
+
+        else:
+            self.timer.stop()
+            self.cap.release()
+            self.start_bt.setText("Start")
 
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    app.exec_()
+    app = QApplication(sys.argv)
+    mainWindow = MainWindow()
+    mainWindow.show()
+    sys.exit(app.exec_())
